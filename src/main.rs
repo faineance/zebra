1622347fce59b5b5473991694d8527f6647359a3
Your Name <you@example.com>
2020-03-21 14:30:43 +0000

    begin parser

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,63 @@
 use std::convert::TryInto;
+use std::fs;
 use z3::ast::Ast;
+extern crate pest;
+#[macro_use]
+extern crate pest_derive;
+#[macro_use]
+extern crate lazy_static;
+use pest::Parser;
+
+use pest::{iterators::*, prec_climber::*};
+
+#[derive(Parser)]
+#[grammar = "grammer.pest"] // relative to src
+struct LangParser;
+
+lazy_static! {
+    static ref PREC_CLIMBER: PrecClimber<Rule> = {
+        use Assoc::*;
+        use Rule::*;
+
+        PrecClimber::new(vec![
+            Operator::new(add, Left) | Operator::new(subtract, Left),
+            Operator::new(multiply, Left) | Operator::new(divide, Left),
+            Operator::new(power, Right),
+        ])
+    };
+}
+
+fn eval(expression: Pairs<Rule>) -> f64 {
+    PREC_CLIMBER.climb(
+        expression,
+        |pair: Pair<Rule>| match pair.as_rule() {
+            Rule::num => pair.as_str().parse::<f64>().unwrap(),
+            Rule::expr => eval(pair.into_inner()),
+            Rule::var => {
+                println!("{}", pair.as_str());
+                unimplemented!()
+            }
+            _ => unreachable!(),
+        },
+        |lhs: f64, op: Pair<Rule>, rhs: f64| match op.as_rule() {
+            Rule::add => lhs + rhs,
+            Rule::subtract => lhs - rhs,
+            Rule::multiply => lhs * rhs,
+            Rule::divide => lhs / rhs,
+            Rule::power => lhs.powf(rhs),
+            _ => unreachable!(),
+        },
+    )
+}
 
 fn main() {
+    let unparsed_file = fs::read_to_string("hole.txt").expect("cannot read file");
+
+    let successful_parse =
+        LangParser::parse(Rule::calculation, &unparsed_file).unwrap_or_else(|e| panic!("{}", e));
+    println!("{}", &successful_parse);
+    println!("{}", eval(successful_parse));
+
     let config = z3::Config::new();
     let ctx = z3::Context::new(&config);
     let solver = z3::Solver::new(&ctx);
